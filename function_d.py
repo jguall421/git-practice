45664f4bfbaf891ebfe6fedeb08d7d2eb5039ee5
Jesica Guallpa <guallpajesica65@gmail.com>
2025-09-22 14:59:19 -0700

    Madi's and Kathryn's code

--- function_d.py
+++ function_d.py
@@ -8,13 +8,11 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-<<<<<<< HEAD
     max = numbers[0]
     for num in numbers:
         if num > max:
             max = num
     return max
-=======
     result = []
     largest = 0
     for item in numbers:
@@ -22,7 +20,6 @@ def max_value(numbers):
             largest = item
     result.append(largest)
     return result
->>>>>>> 92fe02f7a8c8cfe59e1c77a7a5b05a67f8ab6f43
 
 
 if __name__ == "__main__":
